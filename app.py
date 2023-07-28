086c2e2b167efc75c1893556d8a3c9e20029edb4
Oni <103526691+RubisRebelle@users.noreply.github.com>
2023-07-28 18:08:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template, session 
 app = Flask(__name__)
+
 @app.route('/', methods= ['GET'])
 def index ():
     return render_template('index.html')
@@ -15,6 +16,8 @@ def product():
 @app.route('/contact', methods= ['GET'])
 def contact():
     return render_template('contact.html')
+if __name__ == '__main__':
+ app.run(debug=True)
 
 
 
